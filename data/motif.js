c0c48a51207bde67712c97b661390a558904b745
Fuze <92236906+NinoNinoNinoling@users.noreply.github.com>
2025-12-12 09:37:21 +0900

    Update motif.js

--- data/motif.js
+++ data/motif.js
@@ -7,16 +7,16 @@ export const motifData = [
         description: "만화 해피 슈가 라이프의 주인공. 달콤한 것, 즉 사랑을 누구보다 더 갈구한다. 아마 어릴 때부터의 영향으로 애정결핍이 있는 듯하다."
     },
     {
-        image: "",
+        image: "https://i.namu.wiki/i/msz3n_5cjASVYDjdIjHxsyywp2u1Ix-bYK99EJNT2DuiAFwWR72BT-4zz42xIJ3ZParlKZPJdLMiQj-tutk2rzvewnCQsauaBJonYYS4SfayCtrY9k7mZrXVZGJHNpfwjJAYrdDjge-bJIP4vVfLkQ.webp",
         title: "유즈리하 이노리",
         source: "길티 크라운",
-        description: ""
+        description: "사이버 아티스트[5] 에고이스트의 보컬. 16세. 장의사의 멤버이기도 하다. 감정이 적은 편(쿨뷰티)으로 청순하고 귀여운 외모에 체형도 상당히 가녀리고 여리여리한 미소녀지만 그 인상과는, 반대로 아무 때나 태연히 권총을 꺼내 사람을 죽일 준비가 되어있는 것 같으며, 성인 남성들에게 둘러싸여도 전부 제압할 정도의 완력을 지닌데다 혼자서 저 눈에 띄고 에로한 복장으로 적의 포화를 뚫고 단독침투가 가능할 정도의 신체능력을 가진 듯 하다. 또한 장의사 내에서는 미인계도 전문인 듯."
     },
     {
-        image: "",
+        image: "https://i.namu.wiki/i/Kat3AvJ-2sOArw4Q4hH0JQzbuGVuUFDPo0bgiHUwZ252S_n0PZjJZE8l98TuKzfbgNpxs37AeGCpE5gOxNMkN1hjIrDSRauw-lv36VWaJfNxv95S9U61WjxyvdTqGQI6bWh-Ks1bkCidNv0vB9tmUg.webp",
         title: "마토 사쿠라",
         source: "Fate Stay Night",
-        description: ""
+        description: "참을성이 강해서 기본적으로 누군가를 원망하는 일이 없고 자책만 쌓여가는 아가씨."
     },
     {
         image: "",
